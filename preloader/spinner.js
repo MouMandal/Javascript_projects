e8579fccebda10cd60955e83fb3213d04e55205e
Mou <madalmou.2004@gmail.com>
2023-08-17 09:24:40 +0530

    files added

---
+++ preloader/spinner.js
@@ -0,0 +1,10 @@
+
+const spinner = document.querySelector(".spinner");
+const spinner1 = setTimeout(() => {
+    spinner.remove();
+}, 1000);
+
+
+// setTimeout(function () {
+//     spinner.remove();
+// },1000)
